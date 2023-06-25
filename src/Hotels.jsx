93abd9b921f29935f70b411ee6d93426aaeb3ef2
Lion2315 <68440234+Lion2315@users.noreply.github.com>
2023-06-25 01:50:51 -0400

    Update Hotels.jsx

--- src/Hotels.jsx
+++ src/Hotels.jsx
@@ -1,68 +1,56 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Hotels.css';
+import hotelimage from '././Assets/hotel-a.jpg';
+import hotelimage2 from '././Assets/hotel-b.jpg';
+import hotelimage3 from '././Assets/hotel-c.jpg';
 
 const Hotels = () => {
-  const [location, setLocation] = useState('');
-  const [checkInDate, setCheckInDate] = useState('');
-  const [checkOutDate, setCheckOutDate] = useState('');
+  const [showHotels, setShowHotels] = useState(false);
 
-  const handleSearch = () => {
-    // Perform search logic based on location, check-in, and check-out date
-    // You can use an API or any other method to fetch and display hotel results
+  const dummyData = [
+    { id: 1, name: 'Hotel A', price: 150, image: hotelimage },
+    { id: 2, name: 'Hotel B', price: 200, image: hotelimage2 },
+    { id: 3, name: 'Hotel C', price: 250, image: hotelimage3 },
+    { id: 4, name: 'Hotel D', price: 180, image: hotelimage },
+    { id: 5, name: 'Hotel E', price: 220, image: hotelimage2 },
+    { id: 6, name: 'Hotel F', price: 190, image: hotelimage3 },
+    // Add more hotel data
+  ];
+
+  const handleSearch = (e) => {
+    // Perform search logic to fetch and display hotels
     // For now, let's simulate hotel results with dummy data
-    const dummyData = [
-      { id: 1, name: 'Hotel A', price: 150, rating: 4 },
-      { id: 2, name: 'Hotel B', price: 200, rating: 5 },
-      { id: 3, name: 'Hotel C', price: 120, rating: 3 },
-    ];
-    // Set the hotel results in the state
-    // You can display this data in the UI in a similar manner as flight results in the Flights component
+    
+    // Update the state to show the hotel results
+    setShowHotels(true);
   };
 
   return (
-    <div>
-      <h2>Hotels and Accommodation</h2>
-
-      <div>
-        <label htmlFor="location">Location:</label>
-        <input
-          type="text"
-          id="location"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
+    <div className="hotels-container">
+      <h2 className="hotels-header">Hotels and Accommodation</h2>
+      <div className="hotels-content">
+        <button className="search-button" onClick={handleSearch}>
+          Search Hotels
+        </button>
+        {showHotels ? (
+          <div className="hotel-grid">
+            {dummyData.map((hotel) => (
+              <div key={hotel.id} className="hotel-card">
+                <img src={hotel.image} alt={hotel.name} className="hotel-image" />
+                <div className="hotel-info">
+                  <h3 className="hotel-name">{hotel.name}</h3>
+                  <p className="hotel-price">Price: ${hotel.price}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : null}
       </div>
-
-      <div>
-        <label htmlFor="checkIn">Check-In:</label>
-        <input
-          type="date"
-          id="checkIn"
-          value={checkInDate}
-          onChange={(e) => setCheckInDate(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="checkOut">Check-Out:</label>
-        <input
-          type="date"
-          id="checkOut"
-          value={checkOutDate}
-          onChange={(e) => setCheckOutDate(e.target.value)}
-        />
-      </div>
-
-      <button onClick={handleSearch}>Search Hotels</button>
-
-      {/* Add hotel results section */}
-      {/* You can use the same approach as the flight results section in the Flights component */}
-      {/* Display the hotel results based on the data fetched from the API or dummy data */}
-      {/* Handle cases when no hotels are available or display a loading indicator during API fetch */}
-
       <Link to="/">Go to Flights</Link>
     </div>
   );
 };
 
 export default Hotels;
+
